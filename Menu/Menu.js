adff969420f8b8abc801544b28490b977b2b7db6
jackcornellober <jackcornellober@gmail.com>
2019-05-15 15:54:17 -0700

    partial stretch done, article creator function unfinished

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,7 +1,12 @@
-
+let num = 0
 const toggleMenu = () => {
   // Toggle the "menu--open" class on your menu refence. 
-  menu.classList.toggle('menu--open')
+  // menu.classList.toggle('menu--open')
+  
+  if (num === 0) {
+   TweenMax.to(menu, 2,{x:0}); num++;}
+  
+  else {TweenMax.to(menu, 2,{x:-450});num--} 
 }
 
 // Start Here: Create a reference to the ".menu" class
@@ -11,3 +16,5 @@ const menuButton = document.querySelector('.menu-button');
 // Using your menuButton reference, add a click handler that calls toggleMenu.
 
 menuButton.addEventListener('click', (event) => toggleMenu())
+
+TweenMax.to(menu, 0,{x:-450})
